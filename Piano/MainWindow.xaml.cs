bdf8a9fc57f6989bef183a10a828bea45a27ac94
jesus.abarron.z <jesus.abarron.z@gmail.com>
2020-03-12 12:47:12 -0700

    se escucha Do

--- Piano/MainWindow.xaml.cs
+++ Piano/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
 namespace Piano
 {
     /// <summary>
@@ -20,9 +23,30 @@ namespace Piano
     /// </summary>
     public partial class MainWindow : Window
     {
+        private WaveOut waveOut;
+        private MixingSampleProvider mixer;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            waveOut = new WaveOut();
+            mixer = new MixingSampleProvider(WaveFormat.CreateIeeeFloatWaveFormat(44100, 1));
+
+            mixer.ReadFully = true;
+            waveOut.Init(mixer);
+            waveOut.Play();
+        }
+
+        private void btnDo_Click(object sender, RoutedEventArgs e)
+        {
+            var nota_do = new SignalGenerator(44100, 1)
+            {
+                Gain = 0.7,
+                Frequency = 261.6,
+                Type = SignalGeneratorType.Sin
+            }.Take(TimeSpan.FromMilliseconds(500));
+            mixer.AddMixerInput(nota_do);
         }
     }
 }
